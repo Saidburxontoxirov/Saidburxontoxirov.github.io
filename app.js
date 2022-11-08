4cbf20443c2cb5550959dbf2503bada71b6b5adf
saidburxontoxirov <19saidburxon96@gmail.com>
2022-11-08 17:50:32 +0500

    fix: get 1 post

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const button = document.querySelector("#json");
 button.addEventListener("click", function () {
 
     const Http = new XMLHttpRequest();
-    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
     Http.open("GET", url);
     Http.send();
 
